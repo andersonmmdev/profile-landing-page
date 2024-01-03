9f03439c090616f9fb3ab703639e4afffeacdfa9
Andersonmdev <andersonmmdev@gmail.com>
2024-01-03 16:03:05 -0300

    Ajustes titulos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,9 @@ const menu = document.querySelector(".menu");
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+window.addEventListener("scroll", function () {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  }
+});
