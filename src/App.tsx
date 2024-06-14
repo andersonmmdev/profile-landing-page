78168317ce70b63403ee844378b78e80f8cb8015
Andersonmdev <andersonmmdev@gmail.com>
2024-06-13 21:57:37 -0300

    Criacao do botao para retornar ao topo da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { About } from './components/About';
 import { Background } from './components/Background';
 import { Projects } from './components/Projects';
 import { Footer } from './components/Footer';
+import { ScrollTopButton } from './components/shared/ScrollTopButton';
 
 export default function App() {
   return (
     <div className="mx-auto max-w-screen-2xl">
       <Header />
-      <main className="my-4 flex flex-col justify-center gap-20 overflow-hidden px-4 sm:mt-8 md:px-8 xl:px-24">
+      <main className="my-3 flex flex-col justify-center gap-8 overflow-hidden px-4 sm:mt-8 sm:gap-12 md:px-8 xl:gap-16 xl:px-24">
         <About />
         <Background />
         <Projects />
+        <ScrollTopButton />
       </main>
       <Footer />
     </div>
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -19,81 +19,85 @@ export function Projects() {
   });
 
   return (
-    <section id="projects" className="flex flex-col gap-10">
-      <h1
-        ref={ref1}
-        className={`items-center text-center font-lora text-4xl font-bold text-cyan-500 lg:text-5xl
+    <section id="projects" className="flex flex-col gap-8 sm:gap-12 xl:gap-16">
+      <div className="flex flex-col gap-8">
+        <h1
+          ref={ref1}
+          className={`items-center text-center font-lora text-4xl font-bold text-cyan-500 lg:text-5xl
           ${inView1 ? 'animate-appear' : 'opacity-0'}`}
-      >
-        Projetos freelance
-      </h1>
-      <div className="mb-4 flex flex-wrap justify-center gap-8">
-        {freelances.map((freelance, index) => (
-          <AnimatedCard
-            key={index}
-            className="min-w-[400px] max-w-[600px] flex-1"
-          >
-            <div className="flex h-full flex-col justify-between gap-3">
-              <div>
-                <h3 className="mt-1 text-2xl font-bold text-cyan-600">
-                  {freelance.title}
-                </h3>
-                <p className="mt-2 text-pretty text-justify text-slate-100">
-                  {freelance.description}
-                </p>
-                <div className="mt-1">
-                  {freelance.stack.map((tech) => (
-                    <Chip key={tech} text={tech} />
+        >
+          Projetos freelance
+        </h1>
+        <div className="mb-4 flex flex-wrap justify-center gap-8">
+          {freelances.map((freelance, index) => (
+            <AnimatedCard
+              key={index}
+              className="min-w-[400px] max-w-[600px] flex-1"
+            >
+              <div className="flex h-full flex-col justify-between gap-3">
+                <div>
+                  <h3 className="mt-1 text-2xl font-bold text-cyan-600">
+                    {freelance.title}
+                  </h3>
+                  <p className="mt-2 text-pretty text-justify text-slate-100">
+                    {freelance.description}
+                  </p>
+                  <div className="mt-1">
+                    {freelance.stack.map((tech) => (
+                      <Chip key={tech} text={tech} />
+                    ))}
+                  </div>
+                </div>
+                <div className="mt-1 flex gap-4">
+                  {freelance.links.map((link, index) => (
+                    <SeeHereButton key={index} href={link} />
                   ))}
                 </div>
               </div>
-              <div className="mt-1 flex gap-4">
-                {freelance.links.map((link, index) => (
-                  <SeeHereButton key={index} href={link} />
-                ))}
-              </div>
-            </div>
-          </AnimatedCard>
-        ))}
+            </AnimatedCard>
+          ))}
+        </div>
       </div>
-      <h1
-        ref={ref2}
-        className={`items-center text-center font-lora text-4xl font-bold text-cyan-500 lg:text-5xl
+      <div className="flex flex-col gap-8">
+        <h1
+          ref={ref2}
+          className={`items-center text-center font-lora text-4xl font-bold text-cyan-500 lg:text-5xl
           ${inView2 ? 'animate-appear' : 'opacity-0'}`}
-      >
-        Projetos pessoais
-      </h1>
-      <div className="mb-4 flex flex-wrap justify-center gap-8">
-        {projects.map((project, index) => (
-          <AnimatedCard
-            key={index}
-            className="min-w-[400px] max-w-[600px] flex-1"
-          >
-            <div className="flex h-full flex-col justify-between gap-3">
-              <div>
-                <h3 className="mt-1 text-2xl font-bold text-cyan-600">
-                  {project.title}
-                </h3>
-                <p className="mt-2 text-justify text-slate-100">
-                  {project.description}
-                </p>
+        >
+          Projetos pessoais
+        </h1>
+        <div className="mb-4 flex flex-wrap justify-center gap-8">
+          {projects.map((project, index) => (
+            <AnimatedCard
+              key={index}
+              className="min-w-[400px] max-w-[600px] flex-1"
+            >
+              <div className="flex h-full flex-col justify-between gap-3">
+                <div>
+                  <h3 className="mt-1 text-2xl font-bold text-cyan-600">
+                    {project.title}
+                  </h3>
+                  <p className="mt-2 text-justify text-slate-100">
+                    {project.description}
+                  </p>
+                </div>
+                <SeeHereButton href={project.link} />
               </div>
-              <SeeHereButton href={project.link} />
-            </div>
-          </AnimatedCard>
-        ))}
-      </div>
-      <div>
-        <p>
-          * Para mais projetos acesse meu perfil no{' '}
-          <a
-            href="/"
-            target="_blank"
-            className="rounded-sm text-cyan-400 focus:border-cyan-300 focus:outline-none focus:ring-2 focus:ring-cyan-300 focus:ring-opacity-50"
-          >
-            GitHub
-          </a>
-        </p>
+            </AnimatedCard>
+          ))}
+        </div>
+        <div>
+          <p>
+            * Para mais projetos acesse meu perfil no{' '}
+            <a
+              href="/"
+              target="_blank"
+              className="rounded-sm text-cyan-400 focus:border-cyan-300 focus:outline-none focus:ring-2 focus:ring-cyan-300 focus:ring-opacity-50"
+            >
+              GitHub
+            </a>
+          </p>
+        </div>
       </div>
     </section>
   );
